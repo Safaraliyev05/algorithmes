bd50d4971d33fd66fa60153ee12efc99eb487e31
Sardor <safaraliyevsardor05@gmail.com>
2025-04-16 11:14:16 +0500

    commit

--- data structure and alg/binary_search.py
+++ data structure and alg/binary_search.py
@@ -1,21 +1,21 @@
 # sorted array, TIME: O(Log(n) + Log(n)) = O(N Log(N))
-l = [1, 9, 3, 0, 7, 8, 14]  # TIME = O(n) linear search
-l.sort()  # TIME = O(N Log(n))
-left, right = 0, len(l) - 1
-
-target = 14
-while left <= right:  # TIME O(Log(n))
-    mid = (left + right) // 2
-    if l[mid] == target:
-        # print(target)
-        break
-    elif l[mid] > target:
-        right = mid - 1
-    else:
-        left = mid + 1
-else:
-    # print("Target not found")
-    pass
+# l = [1, 9, 3, 0, 7, 8, 14]  # TIME = O(n) linear search
+# l.sort()  # TIME = O(N Log(n))
+# left, right = 0, len(l) - 1
+#
+# target = 14
+# while left <= right:  # TIME O(Log(n))
+#     mid = (left + right) // 2
+#     if l[mid] == target:
+#         # print(target)
+#         break
+#     elif l[mid] > target:
+#         right = mid - 1
+#     else:
+#         left = mid + 1
+# else:
+#     # print("Target not found")
+#     pass
 
 
 # 2824
@@ -45,41 +45,78 @@ else:
 # print(hi.countPairs(nums=[-1, 1, 2, 3, 1], target=2))
 
 # 2089
-class Solution(object):
-    def targetIndices(self, nums, target):
-        nums.sort()
-        result = []
-        # for i, num in enumerate(nums):
-        #     if num == target:
-        #         result.append(i)
-        # return result
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
+# class Solution(object):
+#     def targetIndices(self, nums, target):
+#         nums.sort()
+#         result = []
+#         # for i, num in enumerate(nums):
+#         #     if num == target:
+#         #         result.append(i)
+#         # return result
+#         left, right = 0, len(nums) - 1
+#         while left <= right:
+#             mid = (left + right) // 2
+#             if nums[mid] < target:
+#                 left = mid + 1
+#             else:
+#                 right = mid - 1
+#
+#         while left < len(nums) and nums[left] == target:
+#             result.append(left)
+#             left += 1
+#
+#         return result
+#
+#
+# hi = Solution()
+# print(hi.targetIndices(nums=[1, 2, 5, 2, 3], target=2))
+# print(hi.targetIndices(nums=[1, 2, 5, 2, 3], target=3))
+# print(hi.targetIndices(nums=[1, 2, 5, 2, 3], target=5))
+#
+#
+# # 349
+# class Solution(object):
+#     def intersection(self, nums1, nums2):
+#         result = []
+#         return list(set(nums1) & set(nums2))
+#
+#
+# hi = Solution()
+# print(hi.intersection(nums1=[1, 2, 2, 1], nums2=[2, 2]))
 
-        while left < len(nums) and nums[left] == target:
-            result.append(left)
-            left += 1
-
-        return result
-
-
-hi = Solution()
-print(hi.targetIndices(nums=[1, 2, 5, 2, 3], target=2))
-print(hi.targetIndices(nums=[1, 2, 5, 2, 3], target=3))
-print(hi.targetIndices(nums=[1, 2, 5, 2, 3], target=5))
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
 
 
-# 349
-class Solution(object):
-    def intersection(self, nums1, nums2):
-        result = []
-        return list(set(nums1) & set(nums2))
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    if key < root.val:
+        root.left = insert(root.left, key)
+    else:
+        root.right = insert(root.right, key)
+    return root
 
 
-hi = Solution()
-print(hi.intersection(nums1=[1, 2, 2, 1], nums2=[2, 2]))
+def search(root, key):
+    if root is None:
+        return False
+    if root.val == key:
+        return True
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+
+root = None
+root = insert(root, 50)
+insert(root, 25)
+insert(root, 70)
+
+if search(root, 25):
+    print("Found")
+else:
+    print("Not Found")
